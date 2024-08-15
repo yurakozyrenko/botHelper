0cc9c98bcfbcbaef4a9ba26c93c62a15be948e21
Yura Kozyrenko <yura.mogilev@mail.ru>
2024-08-15 22:50:56 +0300

    add ev file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import config from './configuration/config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot({ load: [config] })],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
+  
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService);
+
+  const HTTP_PORT = configService.get('HTTP_PORT');
+
+  await app.listen(HTTP_PORT, () => {
+    console.log(`🚀 Server listening ${HTTP_PORT} `);
+  });
 }
 bootstrap();
